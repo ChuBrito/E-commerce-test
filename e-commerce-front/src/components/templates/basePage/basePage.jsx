43602b46d0ff66476f869c3ea2d6fc0361491060
Felipe Brito <brito.feh@gmail.com>
2022-08-25 22:23:05 -0300

    adicionado exemplo de input

--- e-commerce-front/src/components/templates/basePage/basePage.jsx
+++ e-commerce-front/src/components/templates/basePage/basePage.jsx
@@ -1,17 +1,66 @@
 // Template de pagina para ser utilizada para testes dos componentes
-import React from "react";
+import React, { useState } from "react";
+import InputField from "../../atoms/inputField/inputField";
 import styles from "./basePage.module.css";
 
-const BasePage = () => (
-  <div className={styles.basePage}>
-    <div className={styles.basePageHeader}>Header da pagina</div>
-    <div className={styles.basePageBody}>
-      <div className={styles.basePageSideMenu}> menu lateral</div>
-      <div className={styles.basePageContainer}>
-        coloque seu componentes da pagina aqui
+const BasePage = () => {
+  const [input1, setInput1] = useState("");
+  const [input2, setInput2] = useState("");
+
+  const onChangeFunc1 = (value) => {
+    setInput1(value);
+    console.log("OnChange ", value);
+  };
+  const onChangeFunc2 = (value) => {
+    setInput2(value);
+    console.log("OnChange2 ", value);
+  };
+  const onSubmitFunc1 = (value) => {
+    setInput1(value);
+    console.log("OnChange1 ", value);
+  };
+  const onSubmitFunc2 = (value) => {
+    console.log("OnSubmit2 ", value);
+  };
+
+  return (
+    <div className={styles.basePage}>
+      <div className={styles.basePageHeader}>Header da pagina</div>
+      <div className={styles.basePageBody}>
+        <div className={styles.basePageSideMenu}> menu lateral</div>
+        <div className={styles.basePageContainer}>
+          coloque seu componentes da pagina aqui
+          <div>
+            <InputField
+              labelText={"Input 1"}
+              value={input1}
+              onChange={onChangeFunc1}
+              onSubmit={onSubmitFunc1}
+              isValid={() => true}
+            />
+          </div>
+          <div>
+            <InputField
+              label={"Input 2"}
+              value={input2}
+              onChange={onChangeFunc2}
+              onSubmit={onSubmitFunc2}
+              isValid={() => false}
+            />
+          </div>
+          <div>
+            <InputField
+              label={"Input 3"}
+              value={input2}
+              onChange={onChangeFunc2}
+              onSubmit={onSubmitFunc2}
+              isValid={() => true}
+            />
+          </div>
+        </div>
       </div>
-    </div>{" "}
-  </div>
-);
+    </div>
+  );
+};
 
 export default BasePage;
